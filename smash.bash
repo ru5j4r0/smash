4c37ff6192c063891680f705bf75cf1e44101c1f
ru5j4r0 <59990307+ru5j4r0@users.noreply.github.com>
2022-06-10 08:59:37 +0600

    add warn about path, more minify

--- smash.bash
+++ smash.bash
@@ -1,22 +1,21 @@
 if test ! -v SMASH_QUIET; then
 echo -e '\033[0;32mStart loading smash...\033[0;0m'
 fi
-if test ! -d ~/.local/share/; then
-mkdir -p ~/.local/share/
-fi
-cd ~/.local/share/ || exit 1
+mkdir -p ~/.local/share/ ~/.local/bin/
+cd ~/.local/share/||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 if test -d smash; then
-cd smash || exit 1
+cd smash||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 if test "$(date +%w)" != "$(cat .smash_last_updated)"; then
-date +%w > .smash_last_updated
+date +%w>.smash_last_updated
 git pull
 fi
 else
 git clone -q git@github.com:ru5j4r0/smash.git
-cd smash || exit 1
-date +%w > .smash_last_updated
+cd smash||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
+date +%w>.smash_last_updated
 chmod 744 bin/*
 ln -s "$PWD"/bin/* "$HOME"/.local/bin/
+echo "$PATH"|grep '\.local/bin'>/dev/null||echo '\033[0;33m [Warn] You should add "~/.local/bin" to your PATH\033[0;0m'
 fi
 if test ! -v SMASH_QUIET; then
 echo -e '\033[0;32mFinished loading smash!\033[0;0m'
