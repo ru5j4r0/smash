d2a5a7617804a87d4285ccad0bd4bbe7048a0032
ru5j4r0 <59990307+ru5j4r0@users.noreply.github.com>
2022-06-14 15:01:11 +0600

    support comparison operator in validate-args

--- modules/validate-args.bash
+++ modules/validate-args.bash
@@ -1,7 +1,16 @@
 import echo-err
 
+declare -Agr SMASH_TEST_OP=(
+	['=']='-eq'
+	['>=']='-ge'
+	['>']='-gt'
+	['<=']='-le'
+	['<']='-lt'
+	['!=']='-ne'
+)
+
 function validate-args() {
-	if test "$SMASH_ARG_COUNT" != "$1"; then
+	if test ! "$SMASH_ARG_COUNT" "${SMASH_TEST_OP[$1]}" "$2"; then
 		echo-err 'Invalid arguments'
 		exit 1
 	fi
