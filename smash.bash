c7f4e33cebba58d66aa81d28b765f6913b889bb5
ru5j4r0 <59990307+ru5j4r0@users.noreply.github.com>
2022-06-14 17:49:36 +0600

    use https to clone, add missing escape flag of echo

--- smash.bash
+++ smash.bash
@@ -3,22 +3,22 @@ echo -e '\033[0;32mStart loading smash...\033[0;0m'
 fi
 SMASH_ORIGINAL_DIR=$PWD
 mkdir -p ~/.local/share/ ~/.local/bin/
-cd ~/.local/share/||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
+cd ~/.local/share/||(echo -e '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 if test -d smash; then
-cd smash||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
+cd smash||(echo -e '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 if test "$(date +%w)" != "$(cat .smash_last_updated)"; then
 date +%w>.smash_last_updated
 git pull -q
 fi
 else
-git clone -q git@github.com:ru5j4r0/smash.git
-cd smash||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
+git clone -q https://github.com/ru5j4r0/smash.git
+cd smash||(echo -e '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 date +%w>.smash_last_updated
 chmod 744 bin/*
 ln -s "$PWD"/bin/* "$HOME"/.local/bin/
-echo "$PATH"|grep '\.local/bin'>/dev/null||echo '\033[0;33m [Warn] You should add "~/.local/bin" to your PATH\033[0;0m'
+echo "$PATH"|grep '\.local/bin'>/dev/null||echo -e '\033[0;33m [Warn] You should add "~/.local/bin" to your PATH\033[0;0m'
 fi
-cd "$SMASH_ORIGINAL_DIR"||(echo '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
+cd "$SMASH_ORIGINAL_DIR"||(echo -e '\033[0;31m [Error] Failed to load smash.\033[0;0m'&&exit 1)
 unset SMASH_ORIGINAL_DIR
 if test ! -v SMASH_QUIET; then
 echo -e '\033[0;32mFinished loading smash!\033[0;0m'
